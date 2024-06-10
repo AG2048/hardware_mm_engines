525be129500728c49ce138809399a7383b93a9d1
AG2048 <45613281+AG2048@users.noreply.github.com>
2024-06-10 17:55:45 -0400

    modified for quartus, added empty registers

--- sum_stationary/sum_stationary.sv
+++ sum_stationary/sum_stationary.sv
@@ -1,62 +1,11 @@
-// This file is public domain, it can be freely copied without restrictions.
-// SPDX-License-Identifier: CC0-1.0
-
 // Matrix Multiplier DUT
-`timescale 1ns/1ps
-
-`ifdef VERILATOR  // make parameter readable from VPI
-  `define VL_RD /*verilator public_flat_rd*/
-`else
-  `define VL_RD
-`endif
-
-// Define
-module processing_unit #(
-  parameter int DATA_WIDTH `VL_RD = 8,
-  parameter int N `VL_RD = 4,
-  parameter int C_DATA_WIDTH = (2 * DATA_WIDTH) + $clog2(N)
-) (
-  input                           clk_i,
-  input                           enable_i,
-  input                           reset_i,
-  input        [DATA_WIDTH-1:0]   west_i,
-  input        [DATA_WIDTH-1:0]   north_i,
-  output       [DATA_WIDTH-1:0]   south_o,
-  output       [DATA_WIDTH-1:0]   east_o,
-  output logic [C_DATA_WIDTH-1:0] result_o
-);
-  logic [C_DATA_WIDTH-1:0] result_calc;
-  logic [C_DATA_WIDTH-1:0] result_reg;
-
-  assign result_calc = north_i * west_i + result_reg;
-  assign result_o = result_reg;
-
-  logic [DATA_WIDTH-1:0] north_i_reg;
-  logic [DATA_WIDTH-1:0] west_i_reg;
-  assign south_o = north_i_reg;
-  assign east_o = west_i_reg;
-
-  always_ff @(posedge clk_i) begin
-    if (reset_i) begin
-      north_i_reg <= '0;
-      west_i_reg <= '0;
-      result_reg <= '0;
-    end else begin
-      if (enable_i) begin
-        result_reg <= result_calc;
-        north_i_reg <= north_i;
-        west_i_reg <= west_i;
-      end
-    end
-  end
-endmodule
 
 module sum_stationary #(
-  parameter int DATA_WIDTH `VL_RD = 8,  // using 8-bit integers 
-  parameter int N `VL_RD = 4, // Computing NxN matrix multiplications
+  parameter int DATA_WIDTH = 8,  // using 8-bit integers 
+  parameter int N = 4, // Computing NxN matrix multiplications
   parameter int C_DATA_WIDTH = (2 * DATA_WIDTH) + $clog2(N)
 ) (
-  input                           clk_i, 
+  input                           clk, 
   input                           reset_i,
   input                           valid_i,
   output logic                    valid_o,  // Output is valid when all data is passed through
@@ -77,7 +26,7 @@ module sum_stationary #(
 
   // Define valid signal
   logic [$clog2(3*N-1)-1:0] counter;
-  always_ff @(posedge clk_i) begin
+  always_ff @(posedge clk) begin
     if (reset_i) begin
       counter <= 3 * N - 2;
     end else if (valid_i || (counter <= 2 * N - 2) && ~valid_o) begin
@@ -97,7 +46,7 @@ module sum_stationary #(
   assign north_inputs[0] = valid_i ? b_i[0] : '0;
 
   // Define shift registers
-  logic [DATA_WIDTH-1:0] west_shift_reg [N-1:1][]; // have registers except to left corner
+  logic [DATA_WIDTH-1:0] west_shift_reg [N-1:1][N-2:0]; // have registers except top left corner
   generate
       genvar i1;
       for (i1 = 1; i1 < N; i1++) begin : shift_reg_gen_west
@@ -105,7 +54,7 @@ module sum_stationary #(
           logic [DATA_WIDTH-1:0] shift_reg_i [i1-1:0]; // each has length 1,2,3,...,N-1
 
           // Implement shift register functionality
-          always_ff @(posedge clk_i) begin
+          always_ff @(posedge clk) begin
               if (reset_i) begin
                   // Reset the shift register
                   for (int j1 = 0; j1 < i1; j1++) begin
@@ -121,12 +70,12 @@ module sum_stationary #(
                   end
               end
           end
-          assign west_shift_reg[i1] = shift_reg_i; // Assign internal shift register to the array
+          assign west_shift_reg[i1][i1-1:0] = shift_reg_i; // Assign internal shift register to the array
           assign west_inputs[i1] = shift_reg_i[i1-1]; // Connect the output
       end
   endgenerate
 
-  logic [DATA_WIDTH-1:0] north_shift_reg [N-1:1][]; // have registers except to left corner
+  logic [DATA_WIDTH-1:0] north_shift_reg [N-1:1][N-2:0]; // have registers except top left corner
   generate
       genvar i2;
       for (i2 = 1; i2 < N; i2++) begin : shift_reg_gen_north
@@ -134,7 +83,7 @@ module sum_stationary #(
           logic [DATA_WIDTH-1:0] shift_reg_i [i2-1:0]; // each has length 1,2,3,...,N-1
 
           // Implement shift register functionality
-          always_ff @(posedge clk_i) begin
+          always_ff @(posedge clk) begin
               if (reset_i) begin
                   // Reset the shift register
                   for (int j2 = 0; j2 < i2; j2++) begin
@@ -150,7 +99,7 @@ module sum_stationary #(
                   end
               end
           end
-          assign north_shift_reg[i2] = shift_reg_i; // Assign internal shift register to the array
+          assign north_shift_reg[i2][i2-1:0] = shift_reg_i; // Assign internal shift register to the array
           assign north_inputs[i2] = shift_reg_i[i2-1]; // Connect the output
       end
   endgenerate
@@ -167,7 +116,7 @@ module sum_stationary #(
           .DATA_WIDTH(DATA_WIDTH),
           .N(N)
         ) u_processing_unit (
-        .clk_i(clk_i),
+        .clk(clk),
         .enable_i(enable),
         .reset_i(reset_i),
         .west_i((j == 0) ? west_inputs[i] : horizontal_interconnect[i][j]),
@@ -179,4 +128,44 @@ module sum_stationary #(
       end
     end
   endgenerate
+endmodule
+
+module processing_unit #(
+  parameter int DATA_WIDTH = 8,
+  parameter int N = 4,
+  parameter int C_DATA_WIDTH = (2 * DATA_WIDTH) + $clog2(N)
+) (
+  input                           clk,
+  input                           enable_i,
+  input                           reset_i,
+  input        [DATA_WIDTH-1:0]   west_i,
+  input        [DATA_WIDTH-1:0]   north_i,
+  output       [DATA_WIDTH-1:0]   south_o,
+  output       [DATA_WIDTH-1:0]   east_o,
+  output logic [C_DATA_WIDTH-1:0] result_o
+);
+  logic [C_DATA_WIDTH-1:0] result_calc;
+  logic [C_DATA_WIDTH-1:0] result_reg;
+
+  assign result_calc = north_i * west_i + result_reg;
+  assign result_o = result_reg;
+
+  logic [DATA_WIDTH-1:0] north_i_reg;
+  logic [DATA_WIDTH-1:0] west_i_reg;
+  assign south_o = north_i_reg;
+  assign east_o = west_i_reg;
+
+  always_ff @(posedge clk) begin
+    if (reset_i) begin
+      north_i_reg <= '0;
+      west_i_reg <= '0;
+      result_reg <= '0;
+    end else begin
+      if (enable_i) begin
+        result_reg <= result_calc;
+        north_i_reg <= north_i;
+        west_i_reg <= west_i;
+      end
+    end
+  end
 endmodule
