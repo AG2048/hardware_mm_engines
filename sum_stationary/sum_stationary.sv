54b6e8334e7ce7fdf3da23e08bc6f498ae8f16f8
AG2048 <45613281+AG2048@users.noreply.github.com>
2024-06-13 14:00:54 -0400

    changed names and added comments

--- sum_stationary/sum_stationary.sv
+++ sum_stationary/sum_stationary.sv
@@ -1,56 +1,46 @@
 // Matrix Multiplier DUT
 
 module sum_stationary #(
-  parameter int DATA_WIDTH = 8,  // using 8-bit integers 
-  parameter int N = 4, // Computing NxN matrix multiplications
-  parameter int C_DATA_WIDTH = (2 * DATA_WIDTH) + $clog2(N)
+  parameter int DATA_WIDTH = 8,   // Using 8-bit integers 
+  parameter int N = 4,            // Computing NxN matrix multiplications
+  parameter int C_DATA_WIDTH = (2 * DATA_WIDTH) + $clog2(N)  // Compute max possible output bits
 ) (
-  input                           clk, 
-  input                           reset_i,
-  input                           valid_i,
-  output logic                    valid_o,  // Output is valid when all data is passed through
-  input        [DATA_WIDTH-1:0]   a_i[N-1:0],
-  input        [DATA_WIDTH-1:0]   b_i[N-1:0],
-  output logic [C_DATA_WIDTH-1:0] c_o[N][N]
+  input                           clk,            // Clock signal
+  input                           reset,          // Reset signal
+  input                           input_valid,    // External input to module is correct/valid
+  output logic                    output_valid,   // Output is valid when all data is passed through
+  input        [DATA_WIDTH-1:0]   a_data[N-1:0],  // Column inputs of A (right to left)
+  input        [DATA_WIDTH-1:0]   b_data[N-1:0],  // Row inputs of B (bottom to top)
+  output logic [C_DATA_WIDTH-1:0] c_data[N][N]    // Full matrix output of C
 );
-  /*
-  define "shift registers" that only gets triggered when enabled (size ranging from 0 to N-1)
-    input of those tied to a_i's units
-    output tied to processing units
-  define array of processing units
-  define an "enable" signal, that is tied to valid_i, 
-  
-  but enable is false when valid_o until reset
-  valid_o tied to a counter of some sort (count from 2N-1 to 0, decrement only when "enable")
-  */
 
   // Define valid signal
-  logic [$clog2(3*N-1)-1:0] counter;
+  logic [$clog2(3*N-1)-1:0] counter;  // Program executes in set number of cycles, count number of cycles
   always_ff @(posedge clk) begin
-    if (reset_i) begin
-      counter <= 3 * N - 2;
-    end else if (valid_i || (counter <= 2 * N - 2) && ~valid_o) begin
+    if (reset) begin
+      counter <= 3 * N - 2;  // N-1 to shift data onto registers, N-1 to pass data through registers, N to compute
+    end else if ((input_valid || (counter <= 2 * N - 2)) && ~output_valid) begin  // Only count when data being input, OR all data is now in registers. Never when output_valid
       counter <= counter - 1;
     end
   end
-  assign valid_o = counter == 0;
+  assign output_valid = counter == 0;
 
   // Define enable signal
-  logic enable;
-  assign enable = valid_i || (counter <= 2 * N - 2) && ~valid_o; // enable to propagate data when: input valid, input filled, output not valid. 
+  logic enable;  // Tells units when to send data to next unit / register
+  assign enable = (input_valid || (counter <= 2 * N - 2)) && ~output_valid;  // Process data when data being input, OR all data is now in registers. Never when output_valid
 
   // Define input data to the unit matrix
-  logic [DATA_WIDTH-1:0] north_inputs [N-1:0];
-  logic [DATA_WIDTH-1:0] west_inputs [N-1:0];
+  logic [DATA_WIDTH-1:0] north_inputs [N-1:0];  // North Inputs have N inputs (B's row)
+  logic [DATA_WIDTH-1:0] west_inputs [N-1:0];  // West Inputs have N inputs (A's column)
   input_delay_register #(
     .DATA_WIDTH(DATA_WIDTH),
     .N(N)
   ) west_delay_register (
     .clk(clk),
-    .reset_i(reset_i),
-    .valid_i(valid_i),
+    .reset(reset),
+    .input_valid(input_valid),
     .enable(enable),
-    .data_i(a_i[N-1:0]),
+    .data_i(a_data[N-1:0]),
     .data_o(west_inputs[N-1:0])
   );
   input_delay_register #(
@@ -58,10 +48,10 @@ module sum_stationary #(
     .N(N)
   ) north_delay_register(
     .clk(clk),
-    .reset_i(reset_i),
-    .valid_i(valid_i),
+    .reset(reset),
+    .input_valid(input_valid),
     .enable(enable),
-    .data_i(b_i[N-1:0]),
+    .data_i(b_data[N-1:0]),
     .data_o(north_inputs[N-1:0])
   );
 
@@ -77,13 +67,13 @@ module sum_stationary #(
           .N(N)
         ) u_processing_unit (
         .clk(clk),
-        .enable_i(enable),
-        .reset_i(reset_i),
+        .enable(enable),
+        .reset(reset),
         .west_i((j == 0) ? west_inputs[i] : horizontal_interconnect[i][j]),
         .north_i((i == 0) ? north_inputs[j] : vertical_interconnect[i][j]),
         .south_o(vertical_interconnect[i+1][j]),
         .east_o(horizontal_interconnect[i][j+1]),
-        .result_o(c_o[i][j])
+        .result_o(c_data[i][j])
         );
       end
     end
@@ -96,33 +86,39 @@ module processing_unit #(
   parameter int N = 4,
   parameter int C_DATA_WIDTH = (2 * DATA_WIDTH) + $clog2(N)
 ) (
-  input                           clk,
-  input                           enable_i,
-  input                           reset_i,
-  input        [DATA_WIDTH-1:0]   west_i,
-  input        [DATA_WIDTH-1:0]   north_i,
-  output       [DATA_WIDTH-1:0]   south_o,
-  output       [DATA_WIDTH-1:0]   east_o,
-  output logic [C_DATA_WIDTH-1:0] result_o
+  input                           clk,      // Clock signal
+  input                           enable,   // Send data to next, and calculate result to store it
+  input                           reset,    // Reset signal
+  input        [DATA_WIDTH-1:0]   west_i,   // West input
+  input        [DATA_WIDTH-1:0]   north_i,  // North input
+  output       [DATA_WIDTH-1:0]   south_o,  // South output
+  output       [DATA_WIDTH-1:0]   east_o,   // East output
+  output logic [C_DATA_WIDTH-1:0] result_o  // Result output
 );
+  // Output is stored in result_reg, while calculation is in result_calc wire before storing
   logic [C_DATA_WIDTH-1:0] result_calc;
   logic [C_DATA_WIDTH-1:0] result_reg;
 
+  // Calculate result comb
   assign result_calc = north_i * west_i + result_reg;
+
+  // Output is always result register
   assign result_o = result_reg;
 
+  // Store value and output through other side
   logic [DATA_WIDTH-1:0] north_i_reg;
   logic [DATA_WIDTH-1:0] west_i_reg;
   assign south_o = north_i_reg;
   assign east_o = west_i_reg;
 
+  // Store data when enable
   always_ff @(posedge clk) begin
-    if (reset_i) begin
+    if (reset) begin
       north_i_reg <= '0;
       west_i_reg <= '0;
       result_reg <= '0;
     end else begin
-      if (enable_i) begin
+      if (enable) begin
         result_reg <= result_calc;
         north_i_reg <= north_i;
         west_i_reg <= west_i;
@@ -133,54 +129,56 @@ endmodule
 
 // Takes a row / col input from a matrix, and delay its input into the actual processing unit for systolic array
 module input_delay_register #(
-  parameter int DATA_WIDTH = 8,
-  parameter int N = 4
+  parameter int DATA_WIDTH = 8,   // Data Width
+  parameter int N = 4             // Matrix Side Length
 ) (
-  input                           clk, 
-  input                           reset_i,
-  input                           valid_i,
-  input                           enable,
-  input        [DATA_WIDTH-1:0]   data_i[N-1:0],
-  output logic [DATA_WIDTH-1:0]   data_o[N-1:0]
+  input                           clk,            // Clock Signal
+  input                           reset,          // Reset signal
+  input                           input_valid,    // Load the data into the registers
+  input                           enable,         // Shift the shift registers
+  input        [DATA_WIDTH-1:0]   data_i[N-1:0],  // Input Data from outside modules
+  output logic [DATA_WIDTH-1:0]   data_o[N-1:0]   // Output Data sent to systolic array units
 );
   // Output data's first value is always direct pass thru
-  assign data_o[0] = valid_i ? data_i[0] : '0;
-  logic [DATA_WIDTH-1:0] shift_reg [N-1:1][N-2:0]; // have registers except top left corner
-  generate
-      genvar i;
-      for (i = 1; i < N; i++) begin : shift_reg_gen
-          // Declare each shift_registers with a specific length
-          logic [DATA_WIDTH-1:0] shift_reg_i [i-1:0]; // each has length 1,2,3,...,N-1
+  assign data_o[0] = input_valid ? data_i[0] : '0;
 
-          // Implement shift register functionality
-          always_ff @(posedge clk) begin
-              if (reset_i) begin
-                  // Reset the shift register
-                  for (int j = 0; j < i; j++) begin
-                      shift_reg_i[j] <= {DATA_WIDTH{1'b0}};
-                  end
-              end else begin
-                  if (enable) begin
-                      // Shift register operation
-                      for (int j = i-1; j > 0; j--) begin
-                          shift_reg_i[j] <= shift_reg_i[j-1];
-                      end
-                      shift_reg_i[0] <= valid_i ? data_i[i] : '0; // Shift in the input
-                  end
-              end
+  // Generate a "straircase" array of shift registers
+  logic [DATA_WIDTH-1:0] shift_reg [N-1:1][N-2:0]; // Have registers except top left corner
+  generate
+    genvar i;
+    for (i = 1; i < N; i++) begin : shift_reg_gen
+      // Declare each shift_registers with a specific length
+      logic [DATA_WIDTH-1:0] shift_reg_i [i-1:0]; // each has length 1,2,3,...,N-1
+
+      // Implement shift register functionality
+      always_ff @(posedge clk) begin
+        if (reset) begin
+          // Reset the shift register
+          for (int j = 0; j < i; j++) begin
+            shift_reg_i[j] <= {DATA_WIDTH{1'b0}};
           end
-          assign shift_reg[i][i-1:0] = shift_reg_i; // Assign internal shift register to the array
-          assign data_o[i] = shift_reg_i[i-1]; // Connect the output
+        end else begin
+          if (enable) begin
+            // Shift register operation
+            for (int j = i-1; j > 0; j--) begin
+              shift_reg_i[j] <= shift_reg_i[j-1];
+            end
+            shift_reg_i[0] <= input_valid ? data_i[i] : '0; // Shift in the input
+          end
+        end
       end
+      assign shift_reg[i][i-1:0] = shift_reg_i; // Assign internal shift register to the array
+      assign data_o[i] = shift_reg_i[i-1]; // Connect the output
+    end
   endgenerate
 endmodule
 
 // Wired to the registers array for 2D matrix output. When values are calculated and if the secondary registers storage is empty:
 // Move all data to this secondary matrix and output data. 
-// TODO: change "valid_o" to result_calculated_valid or something
+// TODO: change "output_valid" to result_calculated_valid or something
 // TODO: device should clear its own "results" registers once value is shifted out. Should not take any new inputs unless results registers is shifted out
 // TODO: I guess have a flag register for (result_empty) and hook that to input ready.
 // TODO: when result is ready (counter went to 0) and output registers is empty, CLEAR EVERYTHING (essentially same as a "rst" signal)
 // TODO: but this "rst" signal does not clear the output registers. (I guess I can just modify each submodule's "rst" to become: (rst || (count==0 && output_empty))
-// Input: NxN array, full signal (which is count == 0 && output_empty, or result_calculated_valid && output_empty), output_ready, out_by_row, reset, clock
+// Input: NxN array, full signal (which is count == 0 && output_empty, or result_valid && output_empty), output_ready, out_by_row, reset, clock
 // Output: a Row/col output of length N, output_empty, output_valid
