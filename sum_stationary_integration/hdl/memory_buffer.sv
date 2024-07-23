26bd5d9cd38fb137006bc3c447ef73ca6ea88256
AG2048 <45613281+AG2048@users.noreply.github.com>
2024-07-23 16:15:49 -0400

    fixed some syntax error in memory buffer

--- sum_stationary_integration/hdl/memory_buffer.sv
+++ sum_stationary_integration/hdl/memory_buffer.sv
@@ -10,9 +10,9 @@ module memory_buffer #(
   parameter int M = 4,                    // This is how much memory is supposed to be stored by buffer (TODO: currently we make it same as N, but will be different)
   parameter int MEMORY_ADDRESS_BITS = 64  // Used to communicate with the memory
   parameter int PARALLEL_DATA_STREAMING_SIZE = 4, // Memory can output 4 numbers at same time TODO: always divisor of SIZE...
-  parameter int MAX_MATRIX_LENGTH = 4096  // Assume the max matrix we will do is 4k
-  parameter int COUNTER_BITS = $clog2(MAX_MATRIX_LENGTH + 1) // We need to keep track of a count from 0 to MAX_MATRIX_LENGTH
-  parameter int MEMORY_INPUT_COUNTER_BITS = $clog2(MAX_MATRIX_LENGTH * N + 1) // For reading from memory, we read at most MAX_MATRIX_LENGTH * N values
+  parameter int MAX_MATRIX_LENGTH = 4096,  // Assume the max matrix we will do is 4k
+  parameter int COUNTER_BITS = $clog2(MAX_MATRIX_LENGTH + 1), // We need to keep track of a count from 0 to MAX_MATRIX_LENGTH
+  parameter int MEMORY_INPUT_COUNTER_BITS = $clog2(MAX_MATRIX_LENGTH * N + 1), // For reading from memory, we read at most MAX_MATRIX_LENGTH * N values
   parameter int REPEATS_COUNTER_BITS = $clog2((MAX_MATRIX_LENGTH/N) + 1) // keep track of how many full data repeats are sent. If we use this for B buffer, the value could become just 1 or 0... (probably keep the bit to a high value in case controller want to fast output A instead of B)
 ) (
   input   logic                           clk,            // Clock signal
